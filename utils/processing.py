dcd6b5f7ba7cbcb262e54bab9be1ae9d3009c043
Vaishvi Jain <vaishvijain24@gmail.com>
2025-05-12 12:28:43 +0530

    Extracting Speaker Embedding via Resemblyzer

--- utils/processing.py
+++ utils/processing.py
@@ -2,6 +2,9 @@ import subprocess
 import os
 import shutil
 
+from resemblyzer import VoiceEncoder, preprocess_wav
+import numpy as np
+
 import torchaudio
 torchaudio.set_audio_backend("soundfile")
 
@@ -30,5 +33,24 @@ def process_audio(input_path, output_path):
 
     shutil.move(vocals_path, output_path)
 
+     # Step 4: Extract speaker embedding
+    embedding_filename = os.path.basename(output_path).replace(".wav", ".npy")
+    embedding_path = os.path.join("processed", embedding_filename)
+    extract_speaker_embedding(output_path, embedding_path)
+
     # Optional cleanup
     os.remove(temp_wav)
+
+
+def extract_speaker_embedding(enhanced_audio_path, save_embedding_path):
+    # Load and preprocess the enhanced audio
+    wav = preprocess_wav(enhanced_audio_path)
+
+    # Load speaker encoder
+    encoder = VoiceEncoder()
+
+    # Get the 128-D embedding
+    embedding = encoder.embed_utterance(wav)
+
+    # Save the embedding to disk
+    np.save(save_embedding_path, embedding)
